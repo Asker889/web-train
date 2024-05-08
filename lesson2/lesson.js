4f8792c3889d0f8a7df3479174267da1269d33a8
Asker Pshunokov <as-0710@mail.ru>
2024-05-08 23:10:50 +0300

    KonNavoz

---
+++ lesson2/lesson.js
@@ -0,0 +1,28 @@
+// const btn_add = document.querySelector('[data-btn="add"]');
+// const btn_remove = document.querySelector('[data-btn="remove"]');
+
+// const app = document.querySelector('#app')
+
+// btn_add.onclick = addblock;
+// btn_remove.onclick = removeBlock;
+
+// function addblock() {
+// 	let block = document.createElement('div');
+// 	block.classList.add('circle');
+// 	block.onclick = toggleCircle;
+// 	app.append(block);
+// }
+
+
+// function removeBlock(){
+// 	let block = app.querySelector('.circle')
+// 	if(block) block.remove();
+// }
+
+// function toggleCircle(e){
+// 	e.target.classList.toggle('active');
+// }
+function pow2() {
+	alert(document.getElementById('inp1').value*document.getElementById('inp1').value);
+	
+}
